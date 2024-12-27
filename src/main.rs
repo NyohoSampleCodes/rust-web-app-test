79fc2ca8f23db038c3d0e240440f76579d0714cd
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-27 23:45:21 +0900

    println debugging

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,14 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+    println!("Start server");
+    
+    let server = HttpServer::new(|| App::new().route("/", web::get().to(hello)))
         .bind("127.0.0.1:8080")?
-        .run()
-        .await
+        .run();
+        
+    println!("Server started.");
+    server.await?;
+    println!("End.....");
+    Ok(())
 }
