41a358512ff3c2c1dc9cf2b82e29f48e509108a1
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-28 00:27:41 +0900

    Introduce OpenTelemetry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use actix_web::{App, HttpServer, Responder, web};
+use opentelemetry::{
+    trace::{Span, Tracer, TracerProvider as _},
+};
+use opentelemetry_sdk::trace::TracerProvider;
 
 async fn hello() -> impl Responder {
     println!("Hello world is going to be shown...");
@@ -7,14 +11,24 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let provider = TracerProvider::builder()
+        .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+        .build();
+    let tracer = provider.tracer("my_tracer");
+
+    let mut span = tracer.start("server_laungh");
+        
     println!("Start server");
-    
+
     let server = HttpServer::new(|| App::new().route("/", web::get().to(hello)))
         .bind("0.0.0.0:8080")?
         .run();
-        
+
+    span.end();
+    
     println!("Server started.");
     server.await?;
     println!("End.....");
+    //provider.shutdown().expect("TracerProvider should shutdown successfully")
     Ok(())
 }
