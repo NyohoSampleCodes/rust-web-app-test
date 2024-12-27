0c1e04eb07d2e1e0f4a85d96406dac894d72eefb
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-12-27 23:58:12 +0900

    Use 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{App, HttpServer, Responder, web};
 
 async fn hello() -> impl Responder {
+    println!("Hello world is going to be shown...");
     "Hello, world!"
 }
 
@@ -9,7 +10,7 @@ async fn main() -> std::io::Result<()> {
     println!("Start server");
     
     let server = HttpServer::new(|| App::new().route("/", web::get().to(hello)))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run();
         
     println!("Server started.");
